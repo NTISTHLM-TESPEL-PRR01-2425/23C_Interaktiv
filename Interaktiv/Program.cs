c5c2fb0399a64f72022207535b215c53c3152303
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-09-05 12:20:46 +0200

    Added some stuff

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Användarnamn:");
+string name = Console.ReadLine();
+name = name.ToLower();
+
+if (name == "micke" || name == "martin")
+{
+  Console.WriteLine("Välkommen!");
+}
+else if (name == "martin")
+{
+  Console.WriteLine("Välkommen!");
+}
+else
+{
+  Console.WriteLine("FU");
+}
+
+Console.ReadLine();
